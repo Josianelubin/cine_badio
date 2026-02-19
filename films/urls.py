24bd50767e7b83dd3ff1ab91d3efa2f9b07a52c4
Josianelubin <ismaelbadio89@gmail.com>
2026-02-18 23:22:33 -0500

    Delete films directory

--- films/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.accueil, name='accueil'),
-    path('films/', views.liste_films, name='liste_films'),
-    path('film/<int:film_id>/', views.detail_film, name='detail_film'),
-    path('rechercher/', views.rechercher_films, name='rechercher'),
-]
